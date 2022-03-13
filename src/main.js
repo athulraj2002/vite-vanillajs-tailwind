cb0eb2398177111e1bfe3c6243114f073c8fe6bd
athulrajneoito <athul.r@neoito.com>
2022-03-13 23:15:45 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import "./style.scss";
+import "../config/config";
+
+console.log(import.meta.env.VITE_SOME_KEY);
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+const config = {
+  root: `${process.cwd()}/src`,
+  envDir:`${process.cwd()}/src/environment`
+};
+export default config;
